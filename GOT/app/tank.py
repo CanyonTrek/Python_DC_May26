1e3eee0690ee4dae1f3109d2701710953b543c60
Donald Cameron <donald.cameron@qa.com>
2026-05-14 23:51:01 +0100

    Updated GOT modules and main program

--- GOT/app/tank.py
+++ GOT/app/tank.py
@@ -1,7 +1,73 @@
 #! /usr/bin/env python3
 # Author: DCameron
 # Version: 1.0
-# Description: This script will demo HOWTO 
+# Description: This module defines a class of Tank
 """ 
-    DocString
+    Tank class for a computer game
 """
+from GOT.app import vehicle # BEST solution for importing within app
+# and also importing from game.py in parent package
+
+class Tank(vehicle.Vehicle):
+    # 2 Components of class: Attribute/Data + Behaviour/Methods
+    def __init__(self, country, model):
+        # CONSTRUCTOR
+        # vehicle.Vehicle.__init__(self, country, model)
+        super().__init__(country, model)
+        self._location = {'x':0, 'y':0, 'z':0}
+        self._direction = 0
+        self._shells = 20
+        self._health = 100
+        # No EXPLICIT return as called IMPLICITLY
+
+
+
+    def rotate_left(self, degrees):
+        self._direction -= degrees % 360
+        return None
+
+    def rotate_right(self, degrees):
+        self._direction += degrees % 360
+        return None
+
+    def shoot(self):
+        self._shells -= 1
+        return None
+
+    def take_damage(self, damage):
+        self._health -= damage
+        return None
+
+    def __del__(self):
+        # DESTRUCTOR
+        print("Boom..Boom..Boom")
+        return None
+
+    # SPECIAL methods..
+    # Example of OPERATOR overloading
+    def __add__(self, other):
+        return self._health + other._health
+
+    # Example of a GETTER and a SETTER
+    def get_health(self):
+        return self._health
+
+    def set_health(self, new_health):
+        self._health = new_health
+        return None
+
+    # WRAP a ONE variable name interface to the getter/setter methods
+    # tank_health = property(get_health, set_health)
+
+    # Alternatively, we could DECORATE our methods with another FUNCTION
+    # DECORATOR is ADDING FUNCTIONALITY from another function to your method
+    @property
+    def tank_health(self):
+        return self._health
+
+    @tank_health.setter
+    def tank_health(self, new_health):
+        self._health = new_health
+        return None
+
+
--- GOT/game.py
+++ GOT/game.py
@@ -3,5 +3,46 @@
 # Version: 1.0
 # Description: This script will demo HOWTO 
 """ 
-    DocString
+    GOT - Game of Tanks
 """
+import sys
+from app import tank
+
+def main():
+    # Instantiate/create 3 new Tank objects
+    jesse_tank = tank.Tank('American', 'Sherman')
+    tyler_tank = tank.Tank('German', 'Tiger')
+    xav_tank = tank.Tank('British', 'Churchill')
+
+    # And the game begins..
+    jesse_tank.accel(63)
+    tyler_tank.accel(34)
+
+    xav_tank.rotate_left(289)
+    xav_tank.accel(29)
+    xav_tank.shoot()
+
+    # And success..
+    jesse_tank.take_damage(58)
+    tyler_tank.take_damage(24)
+
+    # And now for some game visuals - well some print statements
+    print(f"Health of Jesse's tank is {jesse_tank._health}") # POOR CODE
+
+    # Example of operator overloading
+    print(f"Health of Jesse's and Tyler's Tank = {jesse_tank + tyler_tank}")
+
+    # Jesse has received a health boost
+    # jesse_tank._health = 100  # POOR CODE
+    # print(f"NEW health of Jesse's tank is {jesse_tank._health}") # POOR CODE
+    jesse_tank.set_health(101) # SETTER method
+    print(f"NEW health of Jesse's tank is {jesse_tank.get_health()}") # GETTER method
+    jesse_tank.tank_health = 102 # special property
+    print(f"NEW health of Jesse's tank is {jesse_tank.tank_health}") # special property
+
+
+    return None
+
+if __name__ == "__main__":
+    main()
+    sys.exit(0)
